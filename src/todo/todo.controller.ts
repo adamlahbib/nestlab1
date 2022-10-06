d12b7cb37060538acea2be27fa90ed825fb0b335
adamlahbib <adem99.lhb@gmail.com>
2022-10-06 22:22:18 +0100

    task 5

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Param, Controller, Get, Post, Delete, Put } from '@nestjs/common';
 import { Model } from './model';
 import { v4 as uuid} from 'uuid';
 
@@ -21,5 +21,26 @@ export class TodoController {
         this.todoList.push(todo);
         return todo;
     } 
+
+    @Get(':id')
+    getTodo(@Param('id') id: string): Model{
+        return this.todoList.find((todo) => todo.id === id);
+    } 
+
+    @Delete(':id')
+    deleteTodo(@Param('id') id: string): Model{
+        const i=this.todoList.findIndex((todo) => todo.id === id);
+        const deleted=this.todoList[i];
+        this.todoList.splice(i, 1);
+        return deleted;
+    }
+
+    @Put(':id')
+    updateTodo(@Param('id') id: string, @Body() body): Model{
+        const i=this.todoList.findIndex((todo) => todo.id === id);
+        this.todoList[i].name=body.title?body.title:this.todoList[i].name;
+        this.todoList[i].description=body.description?body.description:this.todoList[i].description;
+        return this.todoList[i];
+    }
 
 }
