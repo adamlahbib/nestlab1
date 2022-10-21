19cfcf8dd1c7f5db33269ea164926a9f42da4de2
adamlahbib <adem.lahbib@insat.ucar.tn>
2022-10-21 09:31:04 +0100

    added exceptions

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Model } from './model';
 import { Status } from './status.enum';
 import { TodoAddDTO } from './todoadd.dto';
@@ -28,11 +28,18 @@ export class TodoService {
     }
 
     get(id: string){
-        return this.todoList.find((todo) => todo.id === id);
+        const item=this.todoList.find((todo) => todo.id === id);
+        if (!item){
+            throw new NotFoundException('Todo not found');
+        }
+        return item;
     }
 
     update(id: string, todo: TodoUpdateDTO){
         const i=this.todoList.findIndex((todo) => todo.id === id);
+        if (i===-1){
+            throw new NotFoundException('Todo not found');
+        }
         this.todoList[i].name=todo.title;
         this.todoList[i].description=todo.description;
         return this.todoList[i];
@@ -40,6 +47,9 @@ export class TodoService {
 
     delete(id: string){
         const i=this.todoList.findIndex((todo) => todo.id === id);
+        if (i===-1){
+            throw new NotFoundException('Todo not found');
+        }
         const deleted=this.todoList[i];
         this.todoList.splice(i, 1);
         return deleted;
